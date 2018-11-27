4aa5738a3732411a543c5f49294e7763343913c6
Claret15 <37543659+Claret15@users.noreply.github.com>
2018-11-27 18:43:50 +0000

    Update database config for Heroku

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql-local'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
